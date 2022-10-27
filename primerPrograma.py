90f98dcec4ff85eb9b20e48622e35324a8ffed5b
CrisIntriago <116392563+CrisIntriago@users.noreply.github.com>
2022-10-26 19:24:29 -0500

    Create primerPrograma.py
    Primera version

---
+++ primerPrograma.py
@@ -0,0 +1,5 @@
+print("Hola mundo este es mi primer proyecto compartido en github")
+listaIntegrantes=["Marcos","Sebastian","Cristian,"Daniel"]
+
+for integrante in listaIntegrantes:
+ print(integrante)
